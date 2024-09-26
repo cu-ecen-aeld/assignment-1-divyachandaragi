caaea87f874f76a0ec94cdbd6c015411d8b3707f
DivyaSC <divyaschandaragi@gmail.com>
2024-09-26 14:00:46 +0200

    Retrigger workflow

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -19,4 +19,5 @@ void test_validate_my_username()
      * config file and my_username() functions are setup properly
      */
     TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(),malloc_username_from_conf_file(),"divyachandaragi");
+    
 }
